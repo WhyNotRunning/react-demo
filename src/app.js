0cdc5b2139524bb446411003690baae283a52df6
pangyalin <397087787@qq.com>
2017-01-18 11:59:44 +0800

    模块别名定义，方便后续直接引用别名，无须多写长长的地址

--- src/app.js
+++ src/app.js
@@ -3,10 +3,10 @@ import $ from 'jquery'
 import React from 'react'
 import ReactDOM from 'react-dom';
 import Hello from './components/hello'
-import Header from './components/header/header'
-import Banner from './components/banner/banner'
+import Header from 'Header'
+import Banner from 'Banner'
 import Container from './components/container/container'
-import PageFooter from './components/footer/footer'
+import PageFooter from 'PageFooter'
 // import CallApply from './function/caller-callee-call-apply'
 import './scss/reactdemo'
 
