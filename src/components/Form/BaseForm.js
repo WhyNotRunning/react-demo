0df2ea8d8d7f653cd461b9edc66c6d4ba7961a9f
pangyalin <397087787@qq.com>
2017-09-08 18:01:20 +0800

    修改文件目录结构

---
+++ src/components/Form/BaseForm.js
@@ -0,0 +1,3 @@
+/**
+ * Created by bbb on 2017/9/8.
+ */
--- src/router/router.js
+++ src/router/router.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
-import Header from './components/header/header'
-import Banner from './components/banner/banner'
-import Container from './components/container/container'
-import PageFooter from './components/footer/footer'
+import React from 'react'
+import { Router, Route, Link, hashHistory, IndexRedirect} from 'react-router'
 
-// import Home from '../pages/Home/Home';
-// import Page1 from '../pages/Page1/Page1';
+const App = React.createClass({/*...*/})
+const About = React.createClass({/*...*/})
+// 等等。
 
+const Users = React.createClass({
+    render() {
+        return (
+			<div>
+				<h1>Users</h1>
+				<div className="master">
+					<ul>
+                        {/* 在应用中用 Link 去链接路由 */}
+                        {this.state.users.map(user => (
+							<li key={user.id}><Link to={`/user/${user.id}`}>{user.name}</Link></li>
+                        ))}
+					</ul>
+				</div>
+				<div className="detail">
+                    {this.props.children}
+				</div>
+			</div>
+        )
+    }
+})
 
+const User = React.createClass({
+    componentDidMount() {
+        this.setState({
+            // 路由应该通过有用的信息来呈现，例如 URL 的参数
+            user: findUserById(this.props.params.userId)
+        })
+    },
+
+    render() {
+        return (
+			<div>
+				<h2>{this.state.user.name}</h2>
+                {/* 等等。 */}
+			</div>
+        )
+    }
+})
+
+// 路由配置说明（你不用加载整个配置，
+// 只需加载一个你想要的根路由，
+// 也可以延迟加载这个配置）。
 const getRouter = () => (
 	<Router>
-		<div>
-			<ul>
-				<li><Link to="/">首页</Link></li>
-				<li><Link to="/page1">Page1</Link></li>
-			</ul>
-			<Switch>
-				<Route exact path="/" component={Home}/>
-				<Route path="/page1" component={Page1}/>
-			</Switch>
-		</div>
+		<Route path="/" component={App}>
+			<Route path="login" component={Login}/>
+			<Route path="users" component={Users}>
+				<Route path="/user/:userId" component={User}/>
+			</Route>
+			<Route path="*" component={NoMatch}/>
+		</Route>
 	</Router>
 );
 
